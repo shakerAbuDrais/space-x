c76b9b9cc7d9f080fa1633f20ffd2efb200bd2bd
shakerAbuDrais <thecoffeiner@gmail.com>
2022-11-17 23:44:19 +0300

    Add testing and style my profile

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -23,7 +23,7 @@ const Dragons = () => {
             <h3>{dragon.name}</h3>
             <span>{dragon.type}</span>
             <p id={dragon.id}>
-              <Badge>
+              <Badge className="big">
                 {dragon.reserved ? 'Reserved' : ''}
               </Badge>
               {dragon.type}
